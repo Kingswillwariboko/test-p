179ffd8a83a4e7d8b885392c683aaba8c5e54cc2
unknown <kingswillwariboko@gmail.com>
2022-07-28 00:05:18 +0100

    another changes

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -1,7 +1,14 @@
 import { Container } from "@material-ui/core";
 import React from "react"
+import { Button } from "@material-ui/core";
 import img from "../../images/Mobile/Group 71.svg"
 import img1 from "../../images/Mobile/Group 73.svg"
+import img2 from "../../images/Mobile/image 2.svg"
+import img3 from "../../images/Mobile/image 3.svg"
+import img4 from "../../images/Mobile/image 5.svg"
+import img5 from "../../images/Mobile/image 6.svg"
+import img6 from "../../images/Mobile/Group 5397.svg"
+import img7 from "../../images/Mobile/Group 47.svg"
 
 import './features.scss'
 
@@ -35,6 +42,65 @@ const  Features = ()=> {
                        <img src={img} alt="" />
                      </div>
                  </div>
+            </Container>
+
+            <Container className="features__mainone">
+              <Button>
+                see more features
+              </Button>
+              <div className="features__mainone-one">
+               <img src={img2} alt=""/>
+               <img src={img3} alt=""/>
+              </div>
+
+              <div className="features__mainone-two">
+               <img src={img4} alt=""/>
+               <img src={img5} alt=""/>
+              </div>
+            </Container>
+
+            <Container className="features__maintwo">
+              <div>
+                <h3>INTEGRATION</h3>
+
+                <h6>200+ educational tools and platform integrations</h6>
+                <p>Schoology has every tool your classroom needs and comes
+                   pre-integrated with more than 200+ tools, 
+                  student information systems (SIS), and education platforms.</p>
+              </div>
+              <Button>
+                see intergration
+              </Button>
+            </Container>
+
+            <Container className="features__maintwo">
+              <div>
+                <h3>INTEGRATION</h3>
+
+                <h6> What They Say?</h6>
+                <p>Contentionary has got more than 100k positive ratings from our users around the world</p>
+
+                <p>Some of the students and teachers were greatly helped by the Contentionary.</p>
+
+                <p>Are you too? Please give your assessment</p>
+              </div>
+              <Button>
+                see intergration
+              </Button>
+            </Container>
+
+            <Container className="features__mainthree">
+              <div>
+                <img src={img6} alt="" />
+              </div>
+
+              <div>
+                <h6>Lastest News and Resources</h6>
+
+                <p>See the developments that have occurred to Contentionary in the world</p>
+
+                <img src={img7} alt=""/>
+              </div>
             </Container>
         </div>
     )
