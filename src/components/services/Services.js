52165c23c76f8e3ba5ef891bab6025317236ec6d
unknown <kingswillwariboko@gmail.com>
2022-07-27 15:40:37 +0100

    another changes

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -17,8 +17,8 @@ const Services = () => {
          </Container>
 
            <div>
-               <Container className="container">
-                  <img src={google} alt="" />
+               <Container className="container main">
+                  <img className="container-img" src={google} alt="" />
                   <h3>Online Billing, <br /> Invoicing, & Contracts</h3>
 
                   <p>Simple and secure control of your organization’s 
@@ -29,9 +29,15 @@ const Services = () => {
            </div>
 
            <div>
-               <div>
-
-               </div>
+               <Container>
+                   <h4>What is contentionary</h4>
+                   <p> Contentionary is a platform that allows educators to 
+                       create online classes whereby they can store the course 
+                       materials online; manage assignments, quizzes and exams; 
+                       monitor due dates; 
+                       grade results and provide students with feedback all in one place.
+                    </p>
+               </Container>
            </div>
        </div>
    )
